564657557cf0bb4106ffeb50f6807eda2d0e2344
yuuhi0320 <tenpra0914@gmail.com>
2020-01-03 01:17:19 +0900

    画面を二つ配置 画像を配置（進む、戻る、再生）

---
+++ SlideshowApp/Controller/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 木村旭 on 2020/01/02.
+//  Copyright © 2020 asahi.kimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    @IBAction func nextAction(_ sender: Any) {
+        
+        
+        
+    }
+    @IBAction func backAction(_ sender: Any) {
+        
+        
+        
+    }
+    @IBAction func playAction(_ sender: Any) {
+        
+        
+        
+    }
+    
+}
--- SlideshowApp/Controller/nextViewController.swift
+++ SlideshowApp/Controller/nextViewController.swift
@@ -1,20 +1,22 @@
 //
-//  ViewController.swift
+//  nextViewController.swift
 //  SlideshowApp
 //
-//  Created by 木村旭 on 2020/01/02.
+//  Created by 木村旭 on 2020/01/03.
 //  Copyright © 2020 asahi.kimura. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class nextViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+
     }
+    
+
 
 
 }
-
